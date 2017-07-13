24b760c8375447fa7e02da02dcd6841e778299d0
Annie <Annie@192.168.1.4>
2017-07-13 15:45:55 -0400

    files are moved

---
+++ testgit/src/testgit1/module1.java
@@ -0,0 +1,12 @@
+package testgit1;
+
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class module1 {
+
+	public static void main(String[] args) {
+		ChromeDriver driver=new ChromeDriver();
+		driver.get("https://www.facebook.com/");
+	}
+
+}
